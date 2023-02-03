f695a61598f3f3b5eabb15ffe450545b03ead6af
EJ Cayabyab <elijahcayabyab@investagrams.com>
2023-02-03 10:25:44 +0800

    Project setup

---
+++ SocialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Spam C. on 2/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
