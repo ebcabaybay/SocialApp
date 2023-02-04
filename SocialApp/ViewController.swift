3c8a1e7713e8e4bc31c3c10a72fac778ae3a49f9
EJ Cayabyab <elijahcayabyab@investagrams.com>
2023-02-04 11:19:29 +0800

    Add Login Screen

--- SocialApp/ViewController.swift
+++ SocialApp/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let main = LoginView()
+        main.modalPresentationStyle = .fullScreen
+        present(main, animated: true)
+    }
 }
 
