4fc7ff6aba2d6335ed3ee84ef7a10fdf605a1f81
EJ Cayabyab <elijahcayabyab@investagrams.com>
2023-02-09 09:05:52 +0800

    Add UI Testing

--- SocialAppUITests/SocialAppUITests.swift
+++ SocialAppUITests/SocialAppUITests.swift
@@ -9,33 +9,38 @@ import XCTest
 
 final class SocialAppUITests: XCTestCase {
 
+    var app: XCUIApplication!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
+        try super.setUpWithError()
         continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
+    
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func test() throws {
         let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        app.textFields["Email"].tap()
+        app.textFields["Email"].typeText("ejtest1@gmail.com")
+        app.secureTextFields["Password"].tap()
+        app.secureTextFields["Password"].typeText("123456")
+        app.buttons["Login"].tap()
+        app.buttons["add"].tap()
+        
+        let whatSNewJuanTextView = app.textViews.containing(.staticText, identifier:"What’s new, Juan?").element
+        whatSNewJuanTextView.tap()
+        whatSNewJuanTextView.typeText("Good morning!")
+        app.navigationBars["Create Post"].buttons["Publish"].tap()
+        XCTAssertTrue(app.buttons["add"].exists)
     }
 }
